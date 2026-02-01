de819ccaa7f5597578f07169136d10f839045a91
d4n13lpro <cdap785@gmail.com>
2026-01-31 22:29:27 -0500

    refactor: centralizar enrutamiento en router.php
    - Se movió la lógica de rutas y la función abort() a router.php.
    - Se corrigió error de renderizado asegurando el uso de etiquetas <?php completas.
    - Se simplificó index.php para delegar el control al nuevo enrutador.
    - Se eliminaron redundancias en la carga de functions.php.

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 require "functions.php";
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-// var_dump($uri);
-
-$routes = [
-    "/app_notas_pract/" => "controllers/index.php",
-    "/app_notas_pract/about" => "controllers/about.php",
-    "/app_notas_pract/contact" => "controllers/contact.php"
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-}
+require "router.php";
---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// var_dump($uri);
+
+$routes = [
+    "/app_notas_pract/" => "controllers/index.php",
+    "/app_notas_pract/about" => "controllers/about.php",
+    "/app_notas_pract/contact" => "controllers/contact.php"
+];
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/$code.php";
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
